b065d5966700ed7fe721bb902b8c737861ba8803
OrestHk <hrynchuk.orest@gmail.com>
2022-08-11 19:39:13 +0200

    vue 3, vite 3

--- vite.config.ts
+++ vite.config.ts
@@ -17,10 +17,6 @@ export default defineConfig({
         environment: "jsdom",
         clearMocks: true,
         globals: true,
-        // Waiting for https://github.com/vuejs/vue-test-utils/issues/1982
-        alias: {
-            vue: "vue/dist/vue.runtime.mjs",
-        },
         coverage: {
             include: ["src/**"],
             extension: [".ts", ".vue"],
